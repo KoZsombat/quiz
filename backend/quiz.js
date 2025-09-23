19e86acfb51e24dbf0db1dc743994436e58a0fc3
KoZsombat <125221225+KoZsombat@users.noreply.github.com>
2025-09-23 22:55:07 +0200

    socket disconnect fix

--- backend/quiz.js
+++ backend/quiz.js
@@ -1,33 +1,25 @@
 module.exports = function quizSocketHandler(io) {
-  const roomUsers = {};
+  const roomUsers = [];
 
   io.on("connection", (socket) => {
-    console.log("Felhasználó csatlakozott:", socket.id);
-
     socket.on("joinRoom", (data) => {
       socket.join(data.roomId);
 
-      if (!roomUsers[data.roomId]) roomUsers[data.roomId] = [];
       console.log(`username: ${data.name}, roomid: ${data.roomId}`)
-      roomUsers[data.roomId].push({ id: socket.id, username: data.name, score: 0 });
+      roomUsers.push({ roomId: data.roomId, userId: socket.id, username: data.name, score: 0 });
 
-      console.log(`${socket.id} joined room ${data.roomId}, users: ${roomUsers[data.roomId].length}`);
+      console.log(`${socket.id} joined room ${data.roomId}, users: ${io.sockets.adapter.rooms.get(data.roomId).size}`);
 
       io.to(data.roomId).emit("usersUpdate", roomUsers[data.roomId]);
     });
 
     socket.on("disconnect", () => {
-      console.log("DC-----------------")
-      socket.rooms.forEach((roomId) => {
-        if (roomUsers[roomId]) {
-          const index = roomUsers[roomId].findIndex(user => user.id === socket.id);
-          if (index !== -1) {
-            roomUsers[roomId].splice(index, 1);
-            console.log(`${socket.id} left room ${roomId}, users: ${roomUsers[roomId].length}`);
-            io.to(roomId).emit("usersUpdate", roomUsers[roomId]);
-          }
-        }
-      });
+      const user = roomUsers.findIndex(user => user.userId === socket.id)
+
+      if (user !== -1) {
+        console.log(`${socket.id} left room ${user.roomId}, users remaining: ${io.sockets.adapter.rooms.get(roomUsers[user].roomId)?.size || 0}`);
+        roomUsers.splice(user, 1)
+      }
     });
   });
 }
